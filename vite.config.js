c07ca91352e408dfd49a3e77b4c6dd17a8cc4724
SufyaanMahomed <sufyaan.mahomed77@gmail.com>
2025-10-25 00:44:40 +0200

    Fixed absolute urls

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ export default defineConfig({
   base: './',  //ensures all links and assets are relative paths
   build: {
     outDir: 'dist', //default output folder
-  },
-  server: {
-    port: 5173, //optional: for local dev
-    open: true,
-  },
+  }
 })
